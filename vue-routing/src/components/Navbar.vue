f85c00153dfbccafcee5353b9cc1c425d14d8b9d
AdnanRasad <arronnno@gmail.com>
2018-12-25 04:39:33 +0600

    Vue JS Router

---
+++ vue-routing/src/components/Navbar.vue
@@ -0,0 +1,35 @@
+<!--Recomended is to use router-link for anchoring, also note tht from style the a{} will also be used for router-link tag-->
+<!--We do data binding in About, when vue see the name its gonna grab the path from index.js, (benifit is if you wanna update the router than its gonna need you to update from index.js only)-->
+<template>
+  <nav class="main-nav">
+    <ul>
+      <li>
+        <router-link to="/">Home</router-link>
+      </li>
+      <li>
+        <router-link :to="{name:'About'}">About</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+
+<script>
+export default {
+  name: "Navbar",
+  data() {
+    return {};
+  }
+};
+</script>
+
+
+<style>
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+a {
+  color: #42b983;
+}
+</style>
